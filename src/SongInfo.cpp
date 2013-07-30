2bb9ee09bdaee44b069005a8458d5b6a3e38e22f
Oleg Kosenkov <oleg@kosenkov.ca>
2013-07-30 17:34:55 -0400

    SongInfo class updated

--- src/SongInfo.cpp
+++ src/SongInfo.cpp
@@ -2,50 +2,159 @@
 
 SongInfo::SongInfo(QObject *parent)
  : QObject(parent),
-   m_modId(0) {
-}
-
-SongInfo::SongInfo(const QString &fileName,
-                   const QString &title,
-                   const QString &hash,
-                   int modId,
-                   QObject *parent)
-    : QObject(parent),
-      m_fileName(fileName),
-      m_title(title),
-      m_hash(hash),
-      m_modId(modId)
-{
+   m_modId(0),
+   m_fileSize(0),
+   m_songLength(0),
+   m_downloads(0),
+   m_favourited(0),
+   m_score(0),
+   m_patterns(0),
+   m_orders(0),
+   m_instruments(0),
+   m_samples(0),
+   m_channels(0) {
 }
 
 QString SongInfo::fileName() const {
     return m_fileName;
 }
 
-QString SongInfo::title() const {
-    return m_title;
-}
-
-QString SongInfo::hash() const {
-    return m_hash;
-}
-
-int SongInfo::modId() const {
-    return m_modId;
-}
-
 void SongInfo::setFileName(const QString &value) {
     m_fileName = value;
+}
+
+QString SongInfo::title() const {
+    return m_title;
 }
 
 void SongInfo::setTitle(const QString &value) {
     m_title = value;
 }
 
+QString SongInfo::hash() const {
+    return m_hash;
+}
+
 void SongInfo::setHash(const QString &value) {
     m_hash = value;
 }
 
+int SongInfo::modId() const {
+    return m_modId;
+}
+
 void SongInfo::setModId(int value) {
     m_modId = value;
+}
+
+int SongInfo::fileSize() const {
+    return m_fileSize;
+}
+
+void SongInfo::setFileSize(int value) {
+    m_fileSize = value;
+}
+
+int SongInfo::songLength() const {
+    return m_songLength;
+}
+
+void SongInfo::setSongLength(int value) {
+    m_songLength = value;
+}
+
+int SongInfo::downloads() const {
+    return m_downloads;
+}
+
+void SongInfo::setDownloads(int value) {
+    m_downloads = value;
+}
+
+int SongInfo::favourited() const {
+    return m_favourited;
+}
+
+void SongInfo::setFavourited(int value) {
+    m_favourited = value;
+}
+
+int SongInfo::score() const {
+    return m_score;
+}
+
+void SongInfo::setScore(int value) {
+    m_score = value;
+}
+
+QString SongInfo::format() const {
+    return m_format;
+}
+
+void SongInfo::setFormat(const QString &value) {
+    m_format = value;
+}
+
+QString SongInfo::tracker() const {
+    return m_tracker;
+}
+
+void SongInfo::setTracker(const QString &value) {
+    m_tracker = value;
+}
+
+QString SongInfo::genre() const {
+    return m_genre;
+}
+
+void SongInfo::setGenre(const QString &value) {
+    m_genre = value;
+}
+
+QString SongInfo::artist() const {
+    return m_artist;
+}
+
+void SongInfo::setArtist(const QString &value) {
+    m_artist = value;
+}
+
+int SongInfo::patterns() const {
+    return m_patterns;
+}
+
+void SongInfo::setPatterns(int value) {
+    m_patterns = value;
+}
+
+int SongInfo::orders() const {
+    return m_orders;
+}
+
+void SongInfo::setOrders(int value) {
+    m_orders = value;
+}
+
+int SongInfo::instruments() const {
+    return m_instruments;
+}
+
+void SongInfo::setInstruments(int value) {
+    m_instruments = value;
+}
+
+int SongInfo::samples() const {
+    return m_samples;
+}
+
+void SongInfo::setSamples(int value) {
+    m_samples = value;
+}
+
+int SongInfo::channels() const {
+    return m_channels;
+}
+
+void SongInfo::setChannels(int value) {
+    m_channels = value;
 }
--- src/SongInfo.hpp
+++ src/SongInfo.hpp
@@ -7,28 +7,82 @@
 class SongInfo : public QObject {
     Q_OBJECT
 
-    Q_PROPERTY(QString title READ title WRITE setTitle FINAL)
     Q_PROPERTY(QString fileName READ fileName WRITE setFileName FINAL)
+    Q_PROPERTY(QString title READ title WRITE setTitle FINAL)
     Q_PROPERTY(QString hash READ hash WRITE setHash FINAL)
     Q_PROPERTY(int modId READ modId WRITE setModId FINAL)
+    Q_PROPERTY(int fileSize READ fileSize WRITE setFileSize FINAL)
+    Q_PROPERTY(int songLength READ songLength WRITE setSongLength FINAL)
+    Q_PROPERTY(int downloads READ downloads WRITE setDownloads FINAL)
+    Q_PROPERTY(int favourited READ favourited WRITE setFavourited FINAL)
+    Q_PROPERTY(int score READ score WRITE setScore FINAL)
+    Q_PROPERTY(QString format READ format WRITE setFormat FINAL)
+    Q_PROPERTY(QString tracker READ tracker WRITE setTracker FINAL)
+    Q_PROPERTY(QString genre READ genre WRITE setGenre FINAL)
+    Q_PROPERTY(QString artist READ artist WRITE setArtist FINAL)
+    Q_PROPERTY(int patterns READ patterns WRITE setPatterns FINAL)
+    Q_PROPERTY(int orders READ orders WRITE setOrders FINAL)
+    Q_PROPERTY(int instruments READ instruments WRITE setInstruments FINAL)
+    Q_PROPERTY(int samples READ samples WRITE setSamples FINAL)
+    Q_PROPERTY(int channels READ channels WRITE setChannels FINAL)
 
 public:
     SongInfo(QObject *parent = 0);
-    SongInfo(const QString &fileName,
-             const QString &title,
-             const QString &hash,
-             int modId,
-             QObject *parent = 0);
+
+    QString fileName() const;
+    void setFileName(const QString &value);
 
     QString title() const;
-    QString fileName() const;
-    QString hash() const;
-    int modId() const;
-
     void setTitle(const QString &value);
-    void setFileName(const QString &value);
+
+    QString hash() const;
     void setHash(const QString &value);
+
+    int modId() const;
     void setModId(int value);
+
+    int fileSize() const;
+    void setFileSize(int value);
+
+    int songLength() const;
+    void setSongLength(int value);
+
+    int downloads() const;
+    void setDownloads(int value);
+
+    int favourited() const;
+    void setFavourited(int value);
+
+    int score() const;
+    void setScore(int value);
+
+    QString format() const;
+    void setFormat(const QString &value);
+
+    QString tracker() const;
+    void setTracker(const QString &value);
+
+    QString genre() const;
+    void setGenre(const QString &value);
+
+    QString artist() const;
+    void setArtist(const QString &value);
+
+    int patterns() const;
+    void setPatterns(int value);
+
+    int orders() const;
+    void setOrders(int value);
+
+    int instruments() const;
+    void setInstruments(int value);
+
+    int samples() const;
+    void setSamples(int value);
+
+    int channels() const;
+    void setChannels(int value);
+
 
 Q_SIGNALS:
 private:
@@ -38,6 +92,20 @@ private:
     QString m_title;
     QString m_hash;
     int m_modId;
+    int m_fileSize;
+    int m_songLength;
+    int m_downloads;
+    int m_favourited;
+    int m_score;
+    QString m_format;
+    QString m_tracker;
+    QString m_genre;
+    QString m_artist;
+    int m_patterns;
+    int m_orders;
+    int m_instruments;
+    int m_samples;
+    int m_channels;
 };
 
 Q_DECLARE_METATYPE(SongInfo*);
