9def33d37380f471c24ca2839022c4b12fd1a1ff
Oleg Kosenkov <oleg@kosenkov.ca>
2013-08-14 13:14:03 -0400

    New catalog.sqlite

--- modarchive/modarchive.py
+++ modarchive/modarchive.py
@@ -1,17 +1,23 @@
 #! /usr/local/bin/python3
+import os
+import sys
 import urllib
 import urllib.request
 import re
 import time
 import random
 
+def get_page(url):
+	response = urllib.request.urlopen(url)
+	html = response.read()
+	html = html.decode('utf-8')
+	return html
+
 def query(query_type):
 	print("# modid|filename|downloads")
 	m=re.compile('href="http://modarchive\\.org/data/downloads\\.php\\?moduleid=(\\d+)#([^\"]+)">Downloaded ([\\d,]+) times!', re.MULTILINE)
 	for page in range(1, 26):
-		response = urllib.request.urlopen('http://modarchive.org/index.php?request=view_chart&query=%s&page=%d#mods' % (query_type, page))
-		html = response.read()
-		html = html.decode('utf-8')
+		html = get_page('http://modarchive.org/index.php?request=view_chart&query=%s&page=%d#mods' % (query_type, page))
 		matches = m.findall(html)
 		for match in matches:
 			print("%s|%s|%s" % (match[0], match[1].lower(), match[2].replace(',','')))
@@ -37,6 +43,78 @@ def artists():
 		for match in matches:
 			print("%s|%s" % (match[0], match[1]))
 
+def all_songs_by_artist(artist_id):
+	html = get_page("http://modarchive.org/modules.php?%d" % artist_id)
+
+	m=re.compile('<option value="(\\d+)" >(\\d+)</option>', re.MULTILINE)
+	max_page = 1
+	for page_num, _ in m.findall(html):
+		max_page = max(max_page, int(page_num))
+
+	artist_name = re.search("<h1 class='title'>(.+)'s Modules</h1>", html, re.MULTILINE)
+
+	print("# artist=%d" % artist_id)
+	print("# name=%s" % artist_name.group(1))
+	print("# pages=%d" % max_page)
+	sys.stdout.flush()
+
+	m1=re.compile('href="module\\.php\\?(\\d+)" title="([^\\"]+)"', re.MULTILINE)
+	m2=re.compile('Rated (\\d+) / 10</i>|(Unrated)</i>', re.MULTILINE)
+	for page_no in range(1, max_page + 1):
+		print("# page=%d" % page_no)
+		time.sleep(random.randint(2,20))
+		html = get_page("http://modarchive.org/index.php?request=view_artist_modules&query=%d&page=%d#mods" %
+						(artist_id, page_no))
+		for (mod_id, fileName),(score,unrated) in zip(m1.findall(html),
+														m2.findall(html)):
+			fileName = fileName.lower()
+			if score:
+				print("%s|%s|%s|%s" % (artist_id, mod_id, fileName, score))
+			else:
+				print("%s|%s|%s|%s" % (artist_id, mod_id, fileName, unrated))
+		sys.stdout.flush()
+
+
+
+def top_artists():
+	print("# artistid|name|score|downloads|rating|avgdownloads")
+
+	id_name=re.compile('<a class="title" href="modules\\.php\\?(\\d+)">([^>]+)</a>', re.MULTILINE)
+	artist_score=re.compile('href="modules\\.php\\?([\\d]+)">Scoring ([\\d.]+) / 10</a>', re.MULTILINE)
+	artist_total=re.compile('src="style/images/icons/information\\.png"> Total downloads: ([\\d.,]+)', re.MULTILINE)
+	artist_ratings=re.compile('src="style/images/icons/information\\.png"> Total ratings: ([\\d.,]+)', re.MULTILINE)
+	artist_downloads=re.compile('src="style/images/icons/information\\.png"> Average Downloads per module: ([\\d.,]+)', re.MULTILINE)
+
+	for page in range(1, 6):
+		url = 'http://modarchive.org/index.php?request=view_chart&query=topartists&page=%d#mods' % page
+		print("# %s" % url)
+		time.sleep(1)
+		response = urllib.request.urlopen(url)
+		html = response.read()
+		html = html.decode('utf-8')
+
+		id_name_matches = id_name.findall(html)
+		artist_score_matches = artist_score.findall(html)
+		artist_total_matches = artist_total.findall(html)
+		artist_ratings_matches = artist_ratings.findall(html)
+		artist_downloads_matches = artist_downloads.findall(html)
+
+		result = []
+		for i in zip(id_name_matches,
+			artist_score_matches,
+			artist_total_matches,
+			artist_ratings_matches,
+			artist_downloads_matches):
+			artist_id, artist_name  = i[0][0], i[0][1]
+			score = int(float(i[1][1]))
+			total = int(i[2].replace(',',''))
+			ratings = int(i[3])
+			downloads = int(i[4].replace(',',''))
+			result.append((artist_id, artist_name, score, total, ratings, downloads))
+
+		for r in result:
+			print("%s|%s|%s|%s|%s|%s" % r)
+
 def most_rated():
 	m1 = re.compile('href="http://modarchive\\.org/data/downloads\\.php\\?moduleid=(\\d+)#([^\"]+)"', re.MULTILINE)
 	m2 = re.compile("href='interactive\\.php\\?request=write_comment_page&amp;query=(\\d+)' class='middle'>Rated</a>([^<]+)</i>", re.MULTILINE)
@@ -50,10 +128,11 @@ def most_rated():
 		5:29,
 		4:20,
 		3:15,
-		2:10}
+		2:10,
+		1:7}
 	d = {}
 	#for score in (10,9,8,7,6,5,4,3):
-	for score in (2,):
+	for score in (1,):
 		for page in range(1, num_pages_by_score[score] + 1):
 			try:
 				url = 'http://modarchive.org/index.php?request=view_by_rating_comments&query=%s&page=%d#mods' % (score, page)
@@ -96,19 +175,21 @@ def most_favourited():
 
 	print("# modid|filename|favourited")
 	d = {}
-	for page in range(1, 55):
+	for page in range(1, 174):
 		url = 'http://modarchive.org/index.php?format=&search_type=&query=&request=view_top_favourites&page=%d#mods' % page
 		print("# %s" % url)
+		sys.stdout.flush()
 
 		response = urllib.request.urlopen(url)
 		html = response.read()
 		html = html.decode('utf-8')
 
+		#time.sleep(random.randint(2,5))
+
 		matches = m1.findall(html)
 		for m in matches:
 			modid = m[0]
 			filename = m[1].lower()
-
 			if modid in d:
 				d[modid]['filename'] = filename
 			else:
@@ -123,7 +204,7 @@ def most_favourited():
 				d[modid]['favourited'] = favourited
 			else:
 				d[modid] = {'favourited': favourited,
-										'modid': modid}
+							'modid': modid}
 
 	for modid, info in d.items():
 		filename, favourited = info['filename'], info['favourited']
@@ -317,14 +398,38 @@ def genres(genre_id):
 			else:
 				d[modid] = {'score': favourited,
 							'modid': modid}
-
 	for modid, info in d.items():
 		filename = info['filename']
 		score = info.get('score','')
 		print("%s|%d|%s|%s" % (modid, genre_id, filename, score))
 
+def download_song(modid, fileName):
+	destFile = os.path.join('zip',fileName)
+	if os.path.exists(destFile):
+		return
+	time.sleep(random.randint(2,5))
+	response = urllib.request.urlopen('http://modarchive.org/data/downloads.php?moduleid=%s' % modid)
+	data = response.read()
+	open(destFile, 'wb').write(data)
 
-#most_rated()
-#most_favourited()
-#genres(105)
-artists()
+def download_missing_songs():
+	lines = open('missing-songs.txt','r').readlines()
+	for line in lines:
+		line = line.rstrip()
+		if line[0] == '#':
+			continue
+		x = line.split('|')
+		modid, fileName = x
+		print('Downloading %s.zip (%s)' % (fileName, modid))
+		download_song(modid, fileName + ".zip")
+
+
+if __name__ == '__main__':
+	#most_rated()
+	#most_favourited()
+	#genres(105)
+	#artists()
+	#top_artists()
+	#all_songs_by_artist(i)
+	download_missing_songs()
+
