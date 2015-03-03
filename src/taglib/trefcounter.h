7d7e71855d1e2aa63a0b6e3708597089eb895e21
Oleg Kosenkov <oleg@kosenkov.ca>
2015-03-03 00:45:02 -0500

    Removed redundant code from taglib

--- src/taglib/trefcounter.h
+++ src/taglib/trefcounter.h
@@ -76,32 +76,11 @@ namespace TagLib
   {
   public:
     RefCounterOld() : refCount(1) {}
-
-#ifdef TAGLIB_ATOMIC_MAC
-    void ref() { OSAtomicIncrement32Barrier(const_cast<int32_t*>(&refCount)); }
-    bool deref() { return ! OSAtomicDecrement32Barrier(const_cast<int32_t*>(&refCount)); }
-    int32_t count() { return refCount; }
-  private:
-    volatile int32_t refCount;
-#elif defined(TAGLIB_ATOMIC_WIN)
-    void ref() { InterlockedIncrement(&refCount); }
-    bool deref() { return ! InterlockedDecrement(&refCount); }
-    long count() { return refCount; }
-  private:
-    volatile long refCount;
-#elif defined(TAGLIB_ATOMIC_GCC)
-    void ref() { __sync_add_and_fetch(&refCount, 1); }
-    bool deref() { return ! __sync_sub_and_fetch(&refCount, 1); }
-    int count() { return refCount; }
-  private:
-    volatile int refCount;
-#else
     void ref() { refCount++; }
     bool deref() { return ! --refCount; }
     int count() { return refCount; }
   private:
     uint refCount;
-#endif
   };
 
 }
