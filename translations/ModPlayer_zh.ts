05e080aae5cb10050ea6ac5e0e62202e03f763c9
Oleg Kosenkov <oleg@kosenkov.ca>
2015-03-05 22:25:58 -0500

    Translations update

--- translations/ModPlayer_zh.ts
+++ translations/ModPlayer_zh.ts
@@ -35,7 +35,7 @@
     <name>AppWorldActionItem</name>
     <message>
         <source>Review in BlackBerry World</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Review in BlackBerry World</translation>
     </message>
 </context>
 <context>
@@ -1201,19 +1201,7 @@
     </message>
     <message>
         <source>Keep screen awake</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Theme</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Bright</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Dark</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Keep screen awake</translation>
     </message>
 </context>
 <context>
@@ -1574,6 +1562,18 @@
     <message>
         <source>Enable Background Animation</source>
         <translation>启用背景动画</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation type="unfinished">Theme</translation>
+    </message>
+    <message>
+        <source>Bright</source>
+        <translation type="unfinished">Bright</translation>
+    </message>
+    <message>
+        <source>Dark</source>
+        <translation type="unfinished">Dark</translation>
     </message>
 </context>
 <context>
